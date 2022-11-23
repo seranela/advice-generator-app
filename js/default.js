0434da0842f6d8f60a2259708ff256bf20a04d90
nerodevo <>
2022-11-22 20:10:54 -0600

    Initial solution

---
+++ js/default.js
@@ -0,0 +1,47 @@
+const adviceNumber = document.getElementById('advice-number');
+const adviceText = document.getElementById('advice-quote');
+const diceButton = document.querySelector('.dice-button');
+
+function getAdvice() {
+  // Get data via Fetch API
+  if (window.fetch) {
+    fetch('https://api.adviceslip.com/advice', { method: 'GET' })
+      .then(response => response.json())
+      .then(response => {
+        adviceNumber.innerHTML = response.slip.id;
+        adviceText.innerHTML = "&ldquo;" + response.slip.advice + "&rdquo;";
+      })
+      .catch(err => console.error(err));
+  } else {
+    // Get data via XMLHttpRequest (Fallback)
+    const sendRequest = (method, url) => {
+      const promise = new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.responseType = 'json';
+        xhr.onload = () => {
+          if (xhr.status >= 400) {
+            reject(xhr.response);
+          }
+          resolve(xhr.response);
+        };
+        xhr.send();
+      });
+      return promise;
+    };
+    const getData = async () => {
+      try {
+        const response = await sendRequest('GET', 'https://api.adviceslip.com/advice');
+        adviceNumber.innerHTML = response.slip.id;
+        adviceText.innerHTML = "&ldquo;" + response.slip.advice + "&rdquo;";
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getData();
+  }
+}
+
+diceButton.addEventListener('click', getAdvice, false);
+
+getAdvice();
